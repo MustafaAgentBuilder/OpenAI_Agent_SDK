88cdf07cdcf7788ccd3b242969b80c631dfaeb7c
Mustafaadeel1 <mustafaadeel989@gmail.com>
2025-07-05 16:46:14 +0500

    " Guardrails  "

--- Guardrails/input_Guardrails.py
+++ Guardrails/input_Guardrails.py
@@ -1,17 +1,24 @@
 from pydantic import BaseModel
-from agents import Agent, Runner, GuardrailFunctionOutput, InputGuardrail, input_guardrail
-import asyncio
-
+from agents import Agent, Runner, GuardrailFunctionOutput, InputGuardrail, input_guardrail , set_tracing_disabled       
+from groq import Groq
+import asyncio , os
+from dotenv import load_dotenv
 # Pydantic model for guardrail output
+set_tracing_disabled(True)
+load_dotenv()
 class MathHomeworkOutput(BaseModel):
     is_math_homework: bool
     reasoning: str
+
+
 
 # Guardrail agent to check if input is math homework
 guardrail_agent = Agent(
     name="Math Homework Check",
     instructions="Check if the user input is asking for math homework help.",
-    output_type=MathHomeworkOutput
+    model = "gemma2-9b-it",
+    output_type=MathHomeworkOutput,
+    
 )
 
 # Guardrail function
@@ -28,7 +35,8 @@ async def math_homework_guardrail(ctx, agent, input_data):
 main_agent = Agent(
     name="Customer Support Agent",
     instructions="You are a customer support agent. Help users with their queries unless it's math homework.",
-    input_guardrails=[InputGuardrail(guardrail_function=math_homework_guardrail)]
+    input_guardrails=[math_homework_guardrail],
+    model="meta-llama/llama-guard-4-12b"
 )
 
 # Main function to run the agent
